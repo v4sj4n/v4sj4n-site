ac2f34d2c1f75c116341c66c715cdfccec2967c9
Vasjan Çupri <vasjancupri04@gmail.com>
2024-04-28 15:19:26 +0200

    Added space to the projects design

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import { Block } from '../components/Block'
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft, FiArrowUpRight, FiCode } from 'react-icons/fi'
 import { projects } from '../projects'
 
 export const Projects = () => {
@@ -34,27 +34,28 @@ export const Projects = () => {
       {projects.map(({ code, description, live, name }) => {
         return (
           <Block className='col-span-12 grid grid-cols-12 gap-4 place-items-center bg-zinc-700/50'>
-            <div className='col-span-12 md:col-span-3 w-full flex flex-col items-center justify-center'>
-              <h1 className='text-4xl md:text-3xl mb-4 text-center'>{name}</h1>
-
-              <div className='flex justify-between  items-center w-full  space-x-2'>
+            <div className='col-span-12 md:col-span-4 w-full flex flex-col items-center justify-center'>
+              <h1 className='text-4xl md:text-3xl uppercase text-center font-light my-6'>
+                {name}
+              </h1>
+            </div>
+            <div className='col-span-12 md:col-span-8 mx-2 my-4'>
+              <p className='text-justify'> {description} </p>
+            </div>
+            <div className='col-span-12 flex justify-between  items-center w-full  space-x-4 md:space-x-2'>
                 <a
-                  className='text-center border border-zinc-100 hover:bg-zinc-100 transition-color duration-200 w-1/2 hover:text-zinc-800 px-3 py-1.5 rounded-md'
+                  className='text-center border border-zinc-100 hover:bg-zinc-100 transition-color duration-200 w-1/2 hover:text-zinc-800 px-3 py-1.5 rounded-md flex items-center justify-center gap-2'
                   href={live}
                 >
-                  See Live
+                  See Live <FiArrowUpRight />
                 </a>
                 <a
-                  className=' text-center border border-zinc-100 transition-color duration-200 hover:bg-zinc-100 hover:text-zinc-800 px-3 py-1.5 rounded-md w-1/2'
+                  className=' text-center border border-zinc-100 transition-color duration-200 hover:bg-zinc-100 hover:text-zinc-800 px-3 py-1.5 rounded-md w-1/2 flex items-center justify-center gap-2'
                   href={code}
                 >
-                  See Code
+                  See Code <FiCode />
                 </a>
               </div>
-            </div>
-            <div className='col-span-12 md:col-span-9 text-pretty'>
-              <p> {description} </p>
-            </div>
           </Block>
         )
       })}
