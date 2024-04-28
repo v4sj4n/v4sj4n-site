ec51aadc8734f1ef2fd4e94453289036fad58077
Vasjan Çupri <vasjancupri04@gmail.com>
2024-04-28 15:19:51 +0200

    Changed the content on projects.ts to be sound better

--- src/projects.ts
+++ src/projects.ts
@@ -3,53 +3,42 @@ type Project = {
   description: string
   live: string
   code: string
-
 }
-export const projects : Project[] = [
+export const projects: Project[] = [
   {
     name: 'Moments',
-    description: `This website connects you with friends, you can open a group and
-    chat while also you can save your group moments in there to remind
-    you and your friends of the good times you have had together. The
-    site uses: Nextjs, Supabase, Clerk`,
+    description: `This platform is all about bringing people together, offering a cozy corner for group chats and a place to save your unforgettable group moments. With tools like Next.js, Supabase, and Clerk, I created a site for friends to stay connected and reminisce about the good times.`,
     live: 'https://moments-vas.vercel.app',
-    code: 'https://github.com/v4sj4n/Moments'
-
+    code: 'https://github.com/v4sj4n/Moments',
   },
   {
     name: 'Vas Weather',
-    description: `This is a weather website I have created using webpack and the API
-    from weatherapi, the code is written in modules which are then
-    bundled through webpack, I have also used the async await function
-    to fetch the data from the API. The website is responsive and it is
-    by default on dark mode.`,
+    description: `VasWeather is my little project to keep you in the know about what's happening outside. Built using webpack and weatherapi, it's my attempt to give you a smooth, dark-mode-friendly experience, no matter what device you're on.`,
     live: 'https://weather-vas.netlify.app',
-    code: 'https://github.com/v4sj4n/weather-vas'
-
+    code: 'https://github.com/v4sj4n/weather-vas',
   },
   {
     name: 'Memory Card',
-    description: `This is a game where you have to pick a grid and click the cards, if you clicked a previously clicked card you lose. I have created this project in 3 frameworks: React, Svelte, Solidjs and Vue. If you want to see the code of all the frameworks click the source code button and switch branches. I have used a custom hook (React and Solidjs) utilizing useEffect/createEffect/onMounted and useState/createSignal/ref to fetch data from an API to use for the cards in the game.`,
+    description: `Ready for a challenge? Memory Card is here to test your memory prowess! I've delved into React, Svelte, Solid.js, and Vue to challenge myself on writing it in different frameworks, throwing in some custom hooks for good measure. It's a fun journey of experimentation and learning, with plenty of surprises along the way.`,
     live: 'https://memory-game-card-vas.netlify.app',
-    code: 'https://github.com/v4sj4n/card-memory-game'
+    code: 'https://github.com/v4sj4n/card-memory-game',
   },
   {
     name: 'Shopping Cart',
-    description: `This website is a simple shopping cart powered by the fakestoreapi, you can add items to your cart and then checkout, whilst the checkout doesn't work it is just a simple website that I have created to learn the basics of React Router Dom while using this project I have learned to use the useOutletContext hook and other hooks from the react-router-dom library.`,
+    description: `Okay, so maybe the checkout button doesn't do much yet, but hey, we all start somewhere, right? SHOPPING CART is my way of dipping my toes into React Router Dom while playing around with the fakestoreapi. It's a work in progress, but I had a great time learning it. DESKTOP FOCUSED UI`,
     live: 'https://shopping-cart-vas.netlify.app',
-    code: 'https://github.com/v4sj4n/shopping-cart'
+    code: 'https://github.com/v4sj4n/shopping-cart',
   },
   {
     name: 'Robofriends',
-    description: `Robofriends is a project I created while learning Web Development in this project I've learned the basics of React like how to create the first project, how to create different components, how to download packages and include them in your project, what's a package.json file and many others.`,
+    description: `Introducing ROBOFRIENDS, my humble beginnings in the world of web development. It's been quite the ride, starting from scratch and piecing together React components. This project holds a special place in my heart, reminding me of how far I've come and how much more there is to learn.`,
     live: 'https://robofriends-vas.netlify.app/',
-    code: 'https://github.com/v4sj4n/robofriends'
-
+    code: 'https://github.com/v4sj4n/robofriends',
   },
   {
     name: 'Tic Tac Toe',
-    description: `This is the famous TicTacToe game made all in vanilla javascript, I have created 2 objects for the players and functions for different purposes like checking if there is a winner, checking if the game is drawn, etc.`,
+    description: `Who doesn't love a good old game of TicTacToe, right? I thought I'd give it a shot in vanilla JavaScript, just for kicks. It's nothing fancy, but sometimes the simplest things are the most fun.`,
     live: 'https://tictactoe-odin-vas.netlify.app',
-    code: 'https://github.com/v4sj4n/tictactoe-odin'
+    code: 'https://github.com/v4sj4n/tictactoe-odin',
   },
 ]
