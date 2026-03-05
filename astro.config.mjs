6c6c14335b2b05816a1b3e5d65d1f33eb07daab9
Vasjan Çupri <vasjancupri04@gmail.com>
2026-03-05 23:57:47 +0100

    config: Add Vite SSR configuration to handle React dependencies and alias `react-dom/server` to `react-dom/server.edge`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,30 @@ import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-	integrations: [react()],
-	output: "server",
-	adapter: cloudflare({
-		imageService: "compile",
-	}),
-	env: {
-		schema: {
-			NOTION_SECRET: envField.string({ context: "server", access: "secret" }),
-			NOTION_DB_ID: envField.string({ context: "server", access: "secret" }),
-			CLOUDFLARE_SITE: envField.string({ context: "client", access: "public" }),
-			CLOUDFLARE_SECRET: envField.string({
-				context: "server",
-				access: "secret",
-			}),
-		},
-	},
+  integrations: [react()],
+  output: "server",
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
+  vite: {
+    ssr: {
+      noExternal: ["react", "react-dom", "react-dom/server.edge"],
+    },
+    resolve: {
+      alias: {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
+  env: {
+    schema: {
+      NOTION_SECRET: envField.string({ context: "server", access: "secret" }),
+      NOTION_DB_ID: envField.string({ context: "server", access: "secret" }),
+      CLOUDFLARE_SITE: envField.string({ context: "client", access: "public" }),
+      CLOUDFLARE_SECRET: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
 });
